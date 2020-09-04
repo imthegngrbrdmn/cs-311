d30fbde2cd9c008cb059c97097e4a6e089558298
Millard Arnold V <clear.the.spam@gmail.com>
2020-09-04 13:42:22 -0800

    Function Templates for ctor, dtor, copy ctor, move ctor, copy assignment, move assignment.

--- Project-1/productorder.h
+++ Project-1/productorder.h
@@ -1,4 +1,21 @@
 #ifndef PRODUCTORDER_H
 #define PRODUCTORDER_H
 
+#include <string>
+#include <cstdint>
+
+class ProductOrder
+{
+public:
+  ProductOrder();
+  ~ProductOrder();
+  ProductOrder(const &ProductOrder other);
+  ProductOrder(&&ProductOrder other);
+  ProductOrder& operator=(const ProductOrder& other);
+  ProductOrder& operator=(ProductOrder&& other);
+private:
+  std::string _name;
+  std::uint_fast64_t _numItems;
+}
+
 #endif
