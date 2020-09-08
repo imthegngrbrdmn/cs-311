a85848608b5b8637cf75e82b2409803d79207c3f
Millard Arnold V <clear.the.spam@gmail.com>
2020-09-07 18:43:08 -0800

    Added Function Bodies

--- Project-01/productorder.cpp
+++ Project-01/productorder.cpp
@@ -1,1 +1,87 @@
 #include "productorder.h"
+
+ProductOrder::ProductOrder()
+	:_name("UNSPECIFIED"), _numItems(0)
+{
+}
+
+ProductOrder::~ProductOrder()
+{
+	delete(this);
+}
+
+ProductOrder::ProductOrder(std::string name, int inventory)
+	: _name(name), _numItems(inventory)
+{
+}
+
+std::string ProductOrder::getName() const
+{
+	return _name;
+}
+
+int ProductOrder::getNumber() const
+{
+	return _numItems;
+}
+
+void ProductOrder::setName(std::string name)
+{
+	_name = name;
+}
+
+void ProductOrder::setNumber(int inventory)
+{
+	_numItems = inventory;
+}
+
+bool ProductOrder::empty() const
+{
+	return _numItems==0;
+}
+
+std::string ProductOrder::toString() const
+{
+	return _name + " (" + std::to_string(_numItems) + ")";
+}
+
+bool ProductOrder::operator==(const ProductOrder& other) const
+{
+	return _name==other.getName() && _numItems==other.getNumber();
+}
+
+bool ProductOrder::operator!=(const ProductOrder& other) const
+{
+	return !(_name == other.getName() && _numItems == other.getNumber());
+}
+
+ProductOrder& ProductOrder::operator++()
+{
+	++_numItems;
+	return *this;
+}
+
+ProductOrder& ProductOrder::operator++(int dummy)
+{
+	auto save = *this;
+	++(*this);
+	return save;
+}
+
+ProductOrder& ProductOrder::operator--()
+{
+	--_numItems;
+	return *this;
+}
+
+ProductOrder& ProductOrder::operator--(int dummy)
+{
+	auto save = *this;
+	--(*this);
+	return save;
+}
+
+std::ostream& operator<<(std::ostream& out, const ProductOrder& p)
+{
+	return out << p.toString();
+}
--- Project-01/productorder.h
+++ Project-01/productorder.h
@@ -8,36 +8,36 @@
 class ProductOrder
 {
 public:
-  ProductOrder();
-  ~ProductOrder();
-  ProductOrder(const ProductOrder & other);
-  ProductOrder(&&ProductOrder other);
-  ProductOrder& operator=(const ProductOrder& other);
-  ProductOrder& operator=(ProductOrder&& other);
+	ProductOrder();
+	~ProductOrder();
+	ProductOrder(const ProductOrder& other);
+	ProductOrder(ProductOrder&& other)=default;
+	ProductOrder& operator=(const ProductOrder& other)=default;
+	ProductOrder& operator=(ProductOrder&& other)=default;
 
-  ProductOrder(std::string name, int inventory);
-  std::string getName();
-  int getNumber();
+	ProductOrder(std::string name, int inventory);
+	std::string getName() const;
+	int getNumber() const;
 
-  void setName(std::string name);
-  void setNumber(int inventory);
+	void setName(std::string name);
+	void setNumber(int inventory);
 
-  bool empty();
+	bool empty() const;
 
-  std::string toString();
+	std::string toString() const;
 
-  bool operator==(const ProductOrder & other);
-  bool operator!=(const ProductOrder & other);
-  
-  ProductOrder & operator++();
-  ProductOrder & operator++([[maybe_unused]] int dummy);
+	bool operator==(const ProductOrder& other) const;
+	bool operator!=(const ProductOrder& other) const;
 
-  ProductOrder & operator--();
-  ProductOrder & operator--([[maybe_unused]] int dummy);
+	ProductOrder& operator++();
+	ProductOrder& operator++([[maybe_unused]] int dummy);
+
+	ProductOrder& operator--();
+	ProductOrder& operator--([[maybe_unused]] int dummy);
 private:
-  std::string _name;
-  std::uint_fast64_t _numItems;
-}
+	std::string _name;
+	std::uint_fast32_t _numItems;
+};
 
 std::ostream & operator<<(std::ostream & out, const ProductOrder & p);
 
