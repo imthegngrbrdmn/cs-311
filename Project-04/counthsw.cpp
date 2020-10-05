8703fd519d6867fc910a57546af8bdc53fdcefad
Millard Arnold V <clear.the.spam@gmail.com>
2020-10-05 11:48:35 -0800

    Initial Commit

---
+++ Project-04/counthsw.cpp
@@ -0,0 +1,6 @@
+#include "counthsw.h"
+
+int countHSW(int dim_x, int dim_y, int hole_x, int hole_y, int start_x, int start_y, int finish_x, int finish_y)
+{
+	return 0;
+}
---
+++ Project-04/counthsw.h
@@ -0,0 +1,8 @@
+#ifndef COUNTHSW_H
+
+int countHSW(int dim_x, int dim_y,
+	int hole_x, int hole_y,
+	int start_x, int start_y,
+	int finish_x, int finish_y);
+
+#endif // !COUNTHSW_H
