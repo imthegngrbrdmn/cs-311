d91e3abc3965b55e5e120fb917033e25ac8550ff
Millard Arnold V <clear.the.spam@gmail.com>
2020-10-06 10:48:21 -0800

    FINISH HSW

--- Project-04/counthsw.cpp
+++ Project-04/counthsw.cpp
@@ -10,7 +10,81 @@
 
 #include "counthsw.h"
 
+//	Documentation in header file
 int countHSW(int dim_x, int dim_y, int hole_x, int hole_y, int start_x, int start_y, int finish_x, int finish_y)
 {
 	return HoleySpiderWalk(dim_x,dim_y,hole_x,hole_y,start_x,start_y,finish_x,finish_y).getResult();
+}
+
+//	Documentation in header file
+void HoleySpiderWalk::countHSW_recurse(const int& curr_x, const int& curr_y, std::vector<std::pair<int, int>> covered)
+{
+	covered.push_back(std::make_pair(curr_x, curr_y));
+
+	/*
+		BASE CASE
+		Checks if all spaces are covered and spider is at the finish position
+		IF both are true, adds one to _result and returns.
+	*/
+	if (covered.size() == _dim_x * _dim_y && curr_x == _finish_x && curr_y == _finish_y)
+	{
+		++_result;
+		return;
+	}
+
+	//	South
+	if (curr_y + 1 < _dim_y &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x, curr_y + 1)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x, curr_y + 1, covered);
+	}
+
+	//	Southwest
+	if (curr_x - 1 >= 0 && curr_y + 1 < _dim_y &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x - 1, curr_y + 1)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x - 1, curr_y + 1, covered);	// SOUTHWEST
+	}
+
+	//	West
+	if (curr_x - 1 >= 0 &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x - 1, curr_y)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x - 1, curr_y, covered);	// WEST
+	}
+
+	//	Northwest
+	if (curr_x - 1 >= 0 && curr_y - 1 >= 0 &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x - 1, curr_y - 1)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x - 1, curr_y - 1, covered);	// NORTHWEST
+	}
+
+	//	North
+	if (curr_y - 1 >= 0 &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x, curr_y - 1)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x, curr_y - 1, covered);	// NORTH
+	}
+
+	//	Northeast
+	if (curr_x + 1 < _dim_x && curr_y - 1 >= 0 &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x + 1, curr_y - 1)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x + 1, curr_y - 1, covered);	// NORTHEAST
+	}
+
+	//	East
+	if (curr_x + 1 < _dim_x &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x + 1, curr_y)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x + 1, curr_y, covered);	// EAST
+	}
+
+	//	Southeast
+	if (curr_x + 1 < _dim_x && curr_y + 1 < _dim_y &&
+		std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x + 1, curr_y + 1)) == std::end(covered))
+	{
+		countHSW_recurse(curr_x + 1, curr_y + 1, covered);	// SOUTHEAST
+	}
 }
--- Project-04/counthsw.h
+++ Project-04/counthsw.h
@@ -10,7 +10,7 @@
 #ifndef COUNTHSW_H
 #define COUNTHSW_H
 
-#include <utility>	// std::pair makes coordinates easier.
+#include <utility>	// std::pair
 #include <vector>	// std::vector
 #include <algorithm>// std::find
 
@@ -32,9 +32,32 @@ int countHSW(int dim_x, int dim_y,
 	int start_x, int start_y,
 	int finish_x, int finish_y);
 
+/*
+	Class HoleySpiderWalk
+	Given a board
+	Calculates the number of paths 
+	and stores them in _result
+
+	Invariants:
+		0	<=	_finish_x	<	dim_x
+		0	<=	_finish_x	<	dim_x
+*/
 class HoleySpiderWalk
 {
+//	Ctor and Dtor
 public:
+	/*
+		Ctor
+		Calculates number of possible Holey Spider Walk
+		paths for given board
+		pre:
+			0	<=	hole_x		<	dim_x
+			0	<=	hole_y		<	dim_y
+			0	<=	start_x		<	dim_x
+			0	<=	start_y		<	dim_x
+			0	<=	finish_x	<	dim_x
+			0	<=	finish_x	<	dim_x
+	*/
 	HoleySpiderWalk(const int dim_x, const int dim_y,
 		const int hole_x, const int hole_y,
 		const int start_x, const int start_y,
@@ -46,72 +69,46 @@ public:
 		countHSW_recurse(start_x,start_y,covered);
 		
 	}
+	~HoleySpiderWalk() { }
+
+//	Big 5 Delete All
 	HoleySpiderWalk() = delete;
 	HoleySpiderWalk(const HoleySpiderWalk& other) = delete;
 	HoleySpiderWalk(HoleySpiderWalk&& other) = delete;
 	HoleySpiderWalk& operator=(const HoleySpiderWalk& other) = delete;
 	HoleySpiderWalk& operator=(HoleySpiderWalk&& other) = delete;
-	~HoleySpiderWalk(){ }
+
+//	Public Member Function
 public:
+	/*
+		Member function getResult
+		returns the number of paths
+	*/
 	int getResult() { return _result; }
-public:
-	void countHSW_recurse(const int& curr_x, const int& curr_y,
-		std::vector<std::pair<int, int>> covered)
-	{
-		covered.push_back(std::make_pair(curr_x, curr_y));
-		if (covered.size() == _dim_x * _dim_y)
-		{
-			if (curr_x == _finish_x && curr_y == _finish_y)
-			{
-				++_result;
-			}
-		}
-		if (curr_y + 1 < _dim_y &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x, curr_y + 1)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x, curr_y + 1, covered);	// SOUTH
-		}
-		if (curr_x - 1 >= 0 && curr_y + 1 < _dim_y &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x - 1, curr_y + 1)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x - 1, curr_y + 1, covered);	// SOUTHWEST
-		}
-		if (curr_x - 1 >= 0 &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x - 1, curr_y)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x - 1, curr_y, covered);	// WEST
-		}
-		if (curr_x - 1 >= 0 && curr_y - 1 >= 0 &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x - 1, curr_y - 1)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x - 1, curr_y - 1, covered);	// NORTHWEST
-		}
-		if (curr_y - 1 >= 0 &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x, curr_y - 1)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x, curr_y - 1, covered);	// NORTH
-		}
-		if (curr_x + 1 < _dim_x && curr_y - 1 >= 0 &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x + 1, curr_y - 1)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x + 1, curr_y - 1, covered);	// NORTHEAST
-		}
-		if (curr_x + 1 < _dim_x &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x + 1, curr_y)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x + 1, curr_y, covered);	// EAST
-		}
-		if (curr_x + 1 < _dim_x && curr_y + 1 < _dim_y &&
-			std::find(std::begin(covered), std::end(covered), std::make_pair(curr_x + 1, curr_y + 1)) == std::end(covered))
-		{
-			countHSW_recurse(curr_x + 1, curr_y + 1, covered);	// SOUTHEAST
-		}
-	}
+
+//	Private Member Function
 private:
+	/*
+		Mmeber funtion countHSW_recurse
+		Implementation in source file
+
+		Calculates number of paths for Holey Spider Walk
+		Checks each space from position and 
+		calls function for each available space
+		BASE CASE: covered.size() == area of board
+		pre:
+			0	<=	curr_x	<	_dim_x
+			0	<=	curr_y	<	_dim_y
+			covered contains unique values
+	*/
+	void countHSW_recurse(const int& curr_x, const int& curr_y,
+		std::vector<std::pair<int, int>> covered);
+
+//	Private Data Members
 	const int _dim_x;
 	const int _dim_y;
 	const int _finish_x;
 	const int _finish_y;
 	int _result = 0;
 };
-#endif // !COUNTHSW_H
+#endif
