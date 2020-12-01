dd3de077b9b512038fca549aa8dd6a83cf191eb0
Millard Arnold V <clear.the.spam@gmail.com>
2020-12-01 12:25:25 -0900

    DONE

--- Project-08/squarer_test.cpp
+++ Project-08/squarer_test.cpp
@@ -7,6 +7,11 @@
 // For Project 8, Exercise B
 // Uses the "doctest" unit-testing framework, version 2
 // Requires doctest.h, squarer.h
+/*
+    Modified:   2020-12-01
+    Author:     Millard A. Arnold V
+    Purpose:    Added doctest test cases for Project 08
+*/
 
 // Includes for code to be tested
 #include "squarer.h"         // For class Squarer
@@ -72,6 +77,69 @@ TEST_CASE("squarer: int")
         REQUIRE(s((int)20000) == 20000 * 20000);
         INFO("46340");
         REQUIRE(s((int)46340) == 46340 * 46340);
+    }
+    SUBCASE("small negative numbers")
+    {
+        INFO("-2 to -20");
+        for (int i = -2; i >= -20; --i)
+        {
+            REQUIRE(s(i) == i * i);
+        }
+    }
+    SUBCASE("large negative numbers")
+    {
+        INFO("1000");
+        REQUIRE(s((int)-1000) == 1000 * 1000);
+        INFO("20000");
+        REQUIRE(s((int)-20000) == 20000 * 20000);
+        INFO("46340");
+        REQUIRE(s((int)-46340) == 46340 * 46340);
+    }
+}
+
+TEST_CASE("squarer: double")
+{
+    Squarer s;
+    SUBCASE("decimal fractions")
+    {
+        INFO("0.5");
+        REQUIRE(s((double)0.5) == (double)0.25);
+        INFO("0.25");
+        REQUIRE(s((double)0.25) == (double)0.25*0.25);
+        INFO("0.125");
+        REQUIRE(s((double)0.125) == (double)0.125*0.125);
+    }
+    SUBCASE("Large numbers with decimal parts")
+    {
+        INFO("127.8");
+        REQUIRE(s((double)127.8) == 127.8 * 127.8);
+        INFO("184.2367");
+        REQUIRE(s((double)184.2367) == 184.2367 * 184.2367);
+    }
+}
+
+TEST_CASE("squarer: float")
+{
+    Squarer s;
+    SUBCASE("small floats")
+    {
+        INFO("0.1");
+        REQUIRE(s((float)0.1) == (float)0.1*(float)0.1);
+        INFO("0.2");
+        REQUIRE(s((float)0.2) == (float)0.2 * (float)0.2);
+        INFO("0.00001");
+        REQUIRE(s((float)0.00001) == (float)0.00001 * (float)0.00001);
+        INFO("0.00000017");
+        REQUIRE(s((float)0.00000017) == (float)0.00000017 * (float)0.00000017);
+    }
+    SUBCASE("large floats")
+    {
+        INFO("100.1");
+        REQUIRE(s((float)100.1) == (float)100.1 * (float)100.1);
+        INFO("124650.11243");
+        REQUIRE(s((float)124650.11243) == (float)124650.11243 * (float)124650.11243);
+        INFO("123.123");
+        REQUIRE(s((float)123.123) == (float)123.123 * (float)123.123);
     }
 }
 
